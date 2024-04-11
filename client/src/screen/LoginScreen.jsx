2eac1220602d1003372a67015a2a614341d0e29c
ManiKanta9578 <manikanta9578@gmail.com>
2024-04-11 20:37:57 +0530

    Login screen: Input fields and buttons are impleted

--- client/src/screen/LoginScreen.jsx
+++ client/src/screen/LoginScreen.jsx
@@ -1,31 +1,16 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import React from "react";
-
-const LoginScreen = () => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.topImageContainer}>
-        <Image
-          source={require("../assets/topVector.png")}
-          style={styles.topImage}
-        />
-      </View>
-      <View style={styles.helloContainer}>
-        <Text style={styles.helloText}>Hello</Text>
-      </View>
-      <View>
-        <Text style={styles.signInText}>Sign in to your account</Text>
-      </View>
-    </View>
-  );
-};
-
-export default LoginScreen;
+import FontAwesome from 'react-native-vector-icons/dist/FontAwesome';
+import Fontisto from 'react-native-vector-icons/dist/Fontisto';
+import AntDesign from 'react-native-vector-icons/dist/AntDesign';
+import LinearGradient from 'react-native-linear-gradient';
+import { useNavigation } from "@react-navigation/native";
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#F5F5F5",
     flex: 1,
+    position: "relative"
   },
   topImageContainer: {},
   topImage: {
@@ -42,6 +27,115 @@ const styles = StyleSheet.create({
   signInText: {
     textAlign: "center",
     fontSize: 18,
-    color:"#262626"
+    color: "#262626",
+    marginBottom: 30,
   },
+  inputContainer: {
+    backgroundColor: "#FFFFFF",
+    flexDirection: 'row',
+    borderRadius: 20,
+    marginHorizontal: 40,
+    elevation: 10,
+    marginVertical: 20,
+    alignItems: "center",
+    height: 50,
+  },
+  inputIcon: {
+    marginLeft: 15,
+    marginRight: 5,
+  },
+  textInput: {
+    flex: 1,
+  },
+  forgotPasswordText: {
+    color: "#BEBEBE",
+    textAlign: "right",
+    width: "90%",
+    fontSize: 15,
+  },
+  signInButtonContainer: {
+    flexDirection: "row",
+    marginTop: 120,
+    width: "90%",
+    justifyContent: 'flex-end',
+  },
+  signIn: {
+    color: "#262626",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  linearGradient: {
+    height: 34,
+    width: 56,
+    borderRadius: 17,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 10,
+  },
+  footerText: {
+    color: "#262626",
+    textAlign: "center",
+    fontSize: 18,
+    marginTop: 120,
+  },
+  leftVectorContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+  },
+  leftVectorImage: {
+    height: 450,
+    width: 250,
+  }
 });
+
+const LoginScreen = () => {
+
+  const navigation = useNavigation();
+
+  const handleRegister = () => {
+    navigation.navigate("Signup");
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topImageContainer}>
+        <Image
+          source={require("../assets/topVector.png")}
+          style={styles.topImage}
+        />
+      </View>
+      <View style={styles.helloContainer}>
+        <Text style={styles.helloText}>Hello</Text>
+      </View>
+      <View>
+        <Text style={styles.signInText}>Sign in to your account</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <FontAwesome name={"user"} size={24} color={"#9A9A9A"} style={styles.inputIcon} />
+        <TextInput style={styles.textInput} placeholder="Username" />
+      </View>
+      <View style={styles.inputContainer}>
+        <Fontisto name={"locked"} size={24} color={"#9A9A9A"} style={styles.inputIcon} />
+        <TextInput style={styles.textInput} placeholder="Password" secureTextEntry />
+      </View>
+      <TouchableOpacity>
+        <Text style={styles.forgotPasswordText}>forgot your password?</Text>
+      </TouchableOpacity>
+      <View style={styles.signInButtonContainer}>
+        <Text style={styles.signIn}>Sign In</Text>
+        <LinearGradient colors={['#F97794', '#623AA2']} style={styles.linearGradient}>
+          <AntDesign name={"arrowright"} size={24} color={"white"} />
+        </LinearGradient>
+      </View>
+      <TouchableOpacity onPress={handleRegister}>
+        <Text style={styles.footerText}>Don't have an account?<Text style={{ textDecorationLine: "underline" }}>Create</Text></Text>
+      </TouchableOpacity>
+      <View style={styles.leftVectorContainer}>
+        <ImageBackground source={require("../assets/leftVector.png")} style={styles.leftVectorImage} />
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
